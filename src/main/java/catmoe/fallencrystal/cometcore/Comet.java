2343f0d1f0b3650720fc746367791c8042fae195
Akkariin <my13794531826@Outlook.com>
2023-04-25 19:22:21 +0800

    Initial Commit

---
+++ src/main/java/catmoe/fallencrystal/cometcore/Comet.java
@@ -0,0 +1,16 @@
+package catmoe.fallencrystal.cometcore;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class Comet extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
